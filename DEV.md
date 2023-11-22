c5a3adeca750384fa3f40153dd20944673d02c7e
Isira Adithya <rp.isira@gmail.com>
2023-11-23 02:40:19 +0530

    Added Password Reset routes;

--- DEV.md
+++ DEV.md
@@ -23,10 +23,19 @@
 *Create a database called **eventhive**; Use MariaDB or MySQL;*
 ```sql
 use eventhive;
+DROP TABLE IF EXISTS user;
 CREATE TABLE user (
   id INT AUTO_INCREMENT PRIMARY KEY,
   username VARCHAR(255) UNIQUE NOT NULL,
   password VARCHAR(255) NOT NULL
+);
+DROP TABLE IF EXISTS passwordresettoken;
+CREATE TABLE passwordresettoken (
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  token VARCHAR(255) NOT NULL,
+  userId INT UNIQUE NOT NULL,
+  createdAt INT NOT NULL,
+  FOREIGN KEY (userId) REFERENCES user(id) ON DELETE CASCADE
 );
 
 ```
--- backend/routes/auth/index.js
+++ backend/routes/auth/index.js
@@ -1,13 +1,14 @@
-import express from 'express'
-import db from '../../modules/db.js';
-import bcrypt, { hash } from 'bcrypt';
+import express from 'express';
+import bcrypt from 'bcrypt';
 import {
     PrismaClient
-} from '../../modules/prima_client/index.js'
+} from '../../modules/prisma_client/index.js'
+import crypto from 'crypto'
 
 const prisma = new PrismaClient()
 const router = express.Router();
 
+// 1. Login endpoint
 router.post('/signup', async (req, res) => {
     try {
         const {
@@ -17,9 +18,11 @@ router.post('/signup', async (req, res) => {
 
         // Check if the username already exists
         const existingUser = await prisma.user.findUnique({
-            where: { username },
+            where: {
+                username
+            },
         });
-  
+
         if (existingUser) {
             return res.status(409).send('Username already exists');
         }
@@ -43,7 +46,7 @@ router.post('/signup', async (req, res) => {
     }
 });
 
-// 4. Login endpoint
+// 2. Login endpoint
 router.post('/login', async (req, res) => {
     try {
         const {
@@ -82,13 +85,118 @@ router.post('/login', async (req, res) => {
     }
 });
 
-// 5. Logout endpoint
+// 3. Logout endpoint
 router.delete('/logout', (req, res) => {
     // Implement logout logic (e.g., invalidate session or JWT)
-    if (req.session.isLoggedIn){
+    if (req.session.isLoggedIn) {
+        req.session.destroy();
         res.status(200).send('Logout successful');
     } else {
         res.status(409).send('You are not logged in');
+    }
+});
+
+// Password reset request
+router.post('/reset-password/request', async (req, res) => {
+    try {
+        const {
+            username
+        } = req.body;
+
+        const user = await prisma.user.findUnique({
+            where: {
+                username
+            },
+        });
+
+        if (!user) {
+            return res.status(404).send('User not found');
+        }
+
+        // Delete old tokens for the user
+        await prisma.passwordResetToken.deleteMany({
+            where: { userId: user.id },
+        });
+
+        // Generate a unique token for the password reset
+        const token = crypto.randomBytes(32).toString('hex');
+
+        const currentTimeStamp = new Date().getTime() / 1000;
+        // Store the token in the database
+        await prisma.passwordResetToken.create({
+            data: {
+                token,
+                userId: user.id,
+                createdAt: currentTimeStamp,
+            },
+        });
+
+        console.log(`[DEV] Password Reset Token for ${username} is ${token}`);
+
+        // :TODO
+        // Send the token to the user (e.g., via email)
+
+        res.status(200).send('Password reset token generated successfully');
+    } catch (error) {
+        console.error(error);
+        res.status(500);
+        res.end();
+    }
+});
+
+// Password reset using the token
+router.post('/reset-password/reset', async (req, res) => {
+    try {
+        const {
+            token,
+            newPassword
+        } = req.body;
+
+        const tokenEntry = await prisma.passwordResetToken.findUnique({
+            where: {
+                token
+            },
+            include: {
+                user: true
+            },
+        });
+
+        if (!tokenEntry || !tokenEntry.user) {
+            return res.status(404).send('Invalid token');
+        }
+
+        // Checking if it is expired
+        const currentTimeStamp = new Date().getTime() / 1000;
+        const differenceInSeconds = currentTimeStamp - tokenEntry.createdAt;
+        const tokenDuration = 60 * 10;
+        if (differenceInSeconds > tokenDuration){
+            res.status(401)
+            return res.send('Token expired')
+        }
+
+        // Update the user's password
+        const hashedPassword = await bcrypt.hash(newPassword, 10);
+        await prisma.user.update({
+            where: {
+                id: tokenEntry.user.id
+            },
+            data: {
+                password: hashedPassword
+            },
+        });
+
+        // Remove the used token from the database
+        await prisma.passwordResetToken.delete({
+            where: {
+                id: tokenEntry.id
+            },
+        });
+
+        res.status(200).send('Password reset successful');
+    } catch (error) {
+        console.error(error);
+        res.status(500);
+        res.end();
     }
 });
 
