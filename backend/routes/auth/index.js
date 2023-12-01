0382a718b8fc6916ba5f0c148126aa9d018e05b6
Isira Adithya <rp.isira@gmail.com>
2023-12-02 02:20:34 +0530

    Fixed /auth route with new database changes

--- backend/routes/auth/index.js
+++ backend/routes/auth/index.js
@@ -15,6 +15,11 @@ const router = express.Router();
 router.post(
     '/signup', 
     body("username").notEmpty().isLength({max: 20}), 
+    body("firstname").notEmpty().isLength({max: 20}), 
+    body("lastname").notEmpty().isLength({max: 20}), 
+    body("email").notEmpty().isLength({max: 32}), 
+    body("phone").isLength({max: 32}).isMobilePhone(), 
+    body("address").notEmpty().isLength({max: 256}), 
     body("password").notEmpty().isLength({min: 8}), 
     body("role").notEmpty(), 
     async (req, res) => {
@@ -26,17 +31,12 @@ router.post(
         }
         
         try {
-            const {
-                username,
-                password,
-                role
-            } = req.body;
 
             var isAccountActive = false;
 
             // Checking the role
-            if (role){
-                switch (role) {
+            if (req.body["role"]){
+                switch (req.body["role"]) {
                     case 'VISITOR':
                         isAccountActive = true;
                         break;
@@ -58,34 +58,41 @@ router.post(
             }
 
             // Check if the username already exists
-            const existingUser = await prisma.user.findUnique({
+            const existingUser = await prisma.user.findFirst({
                 where: {
-                    username
-                },
-            });
+                    OR: [
+                        {user_name: req.body["username"]},
+                        {email_address: req.body["email"]},
+                        {phone_number: req.body["phone"]}
+                    ]
+                }
+            })
 
             if (existingUser) {
-                return res.status(409).send('Username already exists');
+                return res.status(409).send('Username/Phone Number/Email Address already exists in our database');
             }
 
             // Hash the password
-            const hashedPassword = await bcrypt.hash(password, 10);
+            const hashedPassword = await bcrypt.hash(req.body["password"], 10);
 
             // Create user using Prisma
             const createdUser = await prisma.user.create({
                 data: {
-                    username,
+                    user_name: req.body["username"],
+                    email_address: req.body["email"],
+                    phone_number: req.body["phone"],
+                    first_name: req.body["firstname"],
+                    last_name: req.body["lastname"],
+                    address: req.body["address"],
                     password: hashedPassword,
-                    role: role,
-                    isActive: isAccountActive
+                    role: req.body["role"],
+                    is_active: isAccountActive,
+                    notification_preference: "ENABLED",
+                    is_verified: false
                 },
             });
 
-            if (role == "EVENT_OWNER"){
-                res.status(201).send('Your account will be enabled after further review');
-            } else {
-                res.status(201).send('User created successfully');
-            }
+            res.status(201).send('User created successfully');
         } catch (error) {
             console.error(error);
             res.status(500);
@@ -106,37 +113,34 @@ router.post(
             return res.send({ errors: result.array() });
         }
         try {
-            
-            const {
-                username,
-                password
-            } = req.body;
 
-            // Fetch user from the database using Prisma
-            const user = await prisma.user.findUnique({
+            const user = await prisma.user.findFirst({
                 where: {
-                    username
-                },
-            });
+                    OR: [
+                        {user_name: req.body["username"]},
+                        {email_address: req.body["username"]},
+                    ]
+                }
+            })
 
             if (!user) {
                 return res.status(401).send('Invalid username or password');
             }
 
-            if (!user.isActive){
+            if (!user.is_active){
                 return res.status(401).send('Account is under review');
             }
 
             // Compare the provided password with the hashed password
-            const passwordMatch = await bcrypt.compare(password, user.password);
+            const passwordMatch = await bcrypt.compare(req.body['password'], user.password);
 
             if (!passwordMatch) {
                 return res.status(401).send('Invalid username or password');
             }
 
             // Store user information in the session
-            req.session.userId = user.id;
-            req.session.username = user.username;
+            req.session.user_id = user.user_id;
+            req.session.username = user.user_name;
             req.session.role = user.role;
             req.session.isLoggedIn = true;
 
@@ -171,13 +175,9 @@ router.post(
             return res.send({ errors: result.array() });
         }
         try {
-            const {
-                username
-            } = req.body;
-
             const user = await prisma.user.findUnique({
                 where: {
-                    username
+                    user_name: req.body['username']
                 },
             });
 
@@ -198,12 +198,12 @@ router.post(
             await prisma.passwordResetToken.create({
                 data: {
                     token,
-                    userId: user.id,
+                    user_id: user.user_id,
                     createdAt: currentTimeStamp,
                 },
             });
 
-            console.log(`[DEV] Password Reset Token for ${username} is ${token}`);
+            console.log(`[DEV] Password Reset Token for ${req.body['username']} is ${token}`);
 
             // TODO:
             // Send the token to the user (e.g., via email)
@@ -229,27 +229,23 @@ router.post(
             return res.send({ errors: result.array() });
         }
         try {
-            const {
-                token,
-                newPassword
-            } = req.body;
 
-            const tokenEntry = await prisma.passwordResetToken.findUnique({
+            const tokenOwner = await prisma.passwordResetToken.findUnique({
                 where: {
-                    token
+                    token: req.body['token']
                 },
                 include: {
                     user: true
                 },
             });
 
-            if (!tokenEntry || !tokenEntry.user) {
+            if (!tokenOwner || !tokenOwner.user) {
                 return res.status(404).send('Invalid token');
             }
 
             // Checking if it is expired
             const currentTimeStamp = new Date().getTime() / 1000;
-            const differenceInSeconds = currentTimeStamp - tokenEntry.createdAt;
+            const differenceInSeconds = currentTimeStamp - tokenOwner.createdAt;
             const tokenDuration = 60 * 10;
             if (differenceInSeconds > tokenDuration){
                 res.status(401)
@@ -257,10 +253,10 @@ router.post(
             }
 
             // Update the user's password
-            const hashedPassword = await bcrypt.hash(newPassword, 10);
+            const hashedPassword = await bcrypt.hash(req.body['newPassword'], 10);
             await prisma.user.update({
                 where: {
-                    id: tokenEntry.user.id
+                    user_id: tokenOwner.user.user_id
                 },
                 data: {
                     password: hashedPassword
@@ -270,7 +266,7 @@ router.post(
             // Remove the used token from the database
             await prisma.passwordResetToken.delete({
                 where: {
-                    id: tokenEntry.id
+                    user_id: tokenOwner.user.user_id
                 },
             });
 
