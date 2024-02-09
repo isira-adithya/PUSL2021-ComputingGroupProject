b14f8cb5edc5c8da3c8f816375c475a1321de8d9
Isira Adithya <rp.isira@gmail.com>
2024-02-10 02:52:10 +0530

    Configured Components and Created Routes

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,4 +1,21 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createRouter, createWebHashHistory } from 'vue-router';
+import { createApp } from 'vue';
 
-createApp(App).mount('#app')
+// Components
+import HomeVue from './Home.vue';
+import LoginVue from './components/Login.vue';
+
+// Routes
+const routes = [
+    { path: '/', component: HomeVue},
+    { path: '/login', component: LoginVue},
+];
+
+const router = createRouter({
+    history: createWebHashHistory(),
+    routes,
+});
+
+const app = createApp(HomeVue);
+app.mount("#app");
+app.use(router);
