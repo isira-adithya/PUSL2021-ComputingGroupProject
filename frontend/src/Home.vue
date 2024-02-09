dc57e7b467871a33866a523f36896a45858bfd27
Isira Adithya <rp.isira@gmail.com>
2024-02-10 02:40:27 +0530

    Renamed App.vue to Home.vue

--- frontend/src/Home.vue
+++ frontend/src/Home.vue
@@ -1,15 +1,12 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
+  name: 'Home',
   components: {
-    HelloWorld
+    
   }
 }
 </script>
