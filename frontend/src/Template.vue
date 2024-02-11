7b0add5cb48d9d77cffa6fe95f060c08c02ffa23
Isira Adithya <rp.isira@gmail.com>
2024-02-11 17:38:55 +0530

    Added Auth WatchDog to the application to make sure user is loggedin when performing visiting authenticated routes

--- frontend/src/Template.vue
+++ frontend/src/Template.vue
@@ -1,69 +1,108 @@
 <template>
   <div>
     <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-    <a class="navbar-brand mx-4" href="#">EventHive</a>
-    <button
-      class="navbar-toggler"
-      type="button"
-      data-toggle="collapse"
-      data-target="#navbarNavAltMarkup"
-      aria-controls="navbarNavAltMarkup"
-      aria-expanded="false"
-      aria-label="Toggle navigation"
-    >
-      <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
-      <div class="navbar-nav">
-        <router-link class="nav-item nav-link" to="/">Home</router-link>
-        <router-link class="nav-item nav-link" to="/login">Login</router-link>
-      </div>
-    </div>
-  </nav>
-
-  <div class="mx-4">
-    <router-view></router-view>
-  </div>
-  
-
-  <div class="mx-4">
-    <footer
-      class="d-flex flex-wrap justify-content-between align-items-center py-3 my-2 border-top"
-    >
-      <p class="col-md-4 mb-0 text-body-secondary">&copy; 2024 EventHive, Inc</p>
-
-      <a
-        href="/"
-        class="col-md-4 d-flex align-items-center justify-content-center mb-3 mb-md-0 me-md-auto link-body-emphasis text-decoration-none"
+      <a class="navbar-brand mx-4" href="#">EventHive</a>
+      <button
+        class="navbar-toggler"
+        type="button"
+        data-toggle="collapse"
+        data-target="#navbarNavAltMarkup"
+        aria-controls="navbarNavAltMarkup"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
       >
-        <svg class="bi me-2" width="40" height="32">
-          <use xlink:href="#bootstrap" />
-        </svg>
-      </a>
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+        <div class="navbar-nav">
+          <router-link class="nav-item nav-link" to="/">Home</router-link>
+          <router-link class="nav-item nav-link" to="/login">Login</router-link>
+        </div>
+      </div>
+    </nav>
 
-      <ul class="nav col-md-4 justify-content-end">
-        <li class="nav-item">
-          <a href="#" class="nav-link px-2 text-body-secondary">Home</a>
-        </li>
-        <li class="nav-item">
-          <a href="#" class="nav-link px-2 text-body-secondary">Features</a>
-        </li>
-        <li class="nav-item">
-          <a href="#" class="nav-link px-2 text-body-secondary">FAQs</a>
-        </li>
-        <li class="nav-item">
-          <a href="#" class="nav-link px-2 text-body-secondary">About</a>
-        </li>
-      </ul>
-    </footer>
-  </div>
+    <div class="mx-4">
+      <router-view></router-view>
+    </div>
+
+    <div class="mx-4">
+      <footer
+        class="d-flex flex-wrap justify-content-between align-items-center py-3 my-2 border-top"
+      >
+        <p class="col-md-4 mb-0 text-body-secondary">
+          &copy; 2024 EventHive, Inc
+        </p>
+
+        <a
+          href="/"
+          class="col-md-4 d-flex align-items-center justify-content-center mb-3 mb-md-0 me-md-auto link-body-emphasis text-decoration-none"
+        >
+          <svg class="bi me-2" width="40" height="32">
+            <use xlink:href="#bootstrap" />
+          </svg>
+        </a>
+
+        <ul class="nav col-md-4 justify-content-end">
+          <li class="nav-item">
+            <a href="#" class="nav-link px-2 text-body-secondary">Home</a>
+          </li>
+          <li class="nav-item">
+            <a href="#" class="nav-link px-2 text-body-secondary">Features</a>
+          </li>
+          <li class="nav-item">
+            <a href="#" class="nav-link px-2 text-body-secondary">FAQs</a>
+          </li>
+          <li class="nav-item">
+            <a href="#" class="nav-link px-2 text-body-secondary">About</a>
+          </li>
+        </ul>
+      </footer>
+    </div>
   </div>
 </template>
   
-  <script>
+<script>
+import axios from "axios";
+import Notiflix from "notiflix";
+
 export default {
   name: "TemplateVue",
   components: {},
+  mounted: () => {
+
+    // Authentication WatchDog
+    window.setInterval(() => {
+      if (
+        document.location.href.includes("/user") ||
+        document.location.href.includes("/eventowner") ||
+        document.location.href.includes("/admin")
+      ) {
+        axios
+          .get("/api/status")
+          .then((data) => {
+            localStorage.setItem("isLoggedIn", JSON.stringify(true));
+          })
+          .catch((err) => {
+            // console.error(err);
+            localStorage.setItem("isLoggedIn", JSON.stringify(false));
+            if (!document.location.href.includes("/login")) {
+              document.location.href = "/#/login";
+              Notiflix.Report.failure(
+                "You've been logged out",
+                "Please sign-in again."
+              );
+            } else {
+              console.log("yellow");
+            }
+          });
+      }
+    }, 5000);
+  },
+  data() {
+    return {
+      isLoggedIn: false,
+    };
+  },
 };
 </script>
   
@@ -73,6 +112,5 @@ export default {
 #app {
   font-family: "Poppins", sans-serif;
 }
-
 </style>
   
