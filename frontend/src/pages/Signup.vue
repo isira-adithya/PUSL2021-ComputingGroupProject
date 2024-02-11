0633c669b1a19f75f98e1dbf0a111156abe966d7
Isira Adithya <rp.isira@gmail.com>
2024-02-11 18:41:22 +0530

    Minor Changes

--- frontend/src/pages/Signup.vue
+++ frontend/src/pages/Signup.vue
@@ -8,73 +8,67 @@
 
           <!-- Email input -->
           <div class="form-outline mb-2">
+            <label class="form-label text-black-50" for="form2Example1">Email address</label>
             <input
               type="email"
-              id="form2Example1"
               class="form-control"
               v-model="email"
             />
-            <label class="form-label" for="form2Example1">Email address</label>
           </div>
 
           <!-- Username input -->
           <div class="form-outline mb-2">
+            <label class="form-label text-black-50" for="form2Example1">Username</label>
             <input
               type="text"
-              id="form2Example1"
               class="form-control"
               v-model="username"
             />
-            <label class="form-label" for="form2Example1">Username</label>
           </div>
 
           <div class="row">
             <div class="col">
               <!-- Firstname input -->
               <div class="form-outline mb-2">
+                <label class="form-label text-black-50" for="form2Example1">First Name</label>
                 <input
                   type="text"
-                  id="form2Example1"
                   class="form-control"
                   v-model="first_name"
                 />
-                <label class="form-label" for="form2Example1">First Name</label>
               </div>
             </div>
             <div class="col">
               <!-- Lastname input -->
               <div class="form-outline mb-2">
+                <label class="form-label text-black-50" for="form2Example1">Last Name</label>
                 <input
                   type="text"
-                  id="form2Example1"
                   class="form-control"
                   v-model="last_name"
                 />
-                <label class="form-label" for="form2Example1">Last Name</label>
               </div>
             </div>
           </div>
 
           <!-- Address input -->
           <div class="form-outline mb-2">
+            <label class="form-label text-black-50" for="form2Example1">Address</label>
             <input
               type="text"
-              id="form2Example1"
               class="form-control"
               v-model="address"
             />
-            <label class="form-label" for="form2Example1">Address</label>
           </div>
 
           <!-- Phone number input -->
           <div class="form-outline mb-2">
+            <label class="form-label text-black-50" for="form2Example1">Phone Number</label>
             <input
               type="text"
-              id="form2Example1"
               class="form-control"
               v-model="phone"
             />
-            <label class="form-label" for="form2Example1">Phone Number</label>
           </div>
 
           <!-- Password inputs -->
@@ -82,25 +76,23 @@
             <div class="col">
               <!-- Password input -->
               <div class="form-outline mb-2">
+                <label class="form-label text-black-50" for="form2Example1">Password</label>
                 <input
                   type="password"
-                  id="form2Example1"
                   class="form-control"
                   v-model="password"
                 />
-                <label class="form-label" for="form2Example1">Password</label>
               </div>
             </div>
             <div class="col">
               <!-- RepeatPassword input -->
               <div class="form-outline mb-2">
+                <label class="form-label text-black-50" for="form2Example1">Repeat Password</label>
                 <input
                   type="password"
-                  id="form2Example1"
                   class="form-control"
                   v-model="repeat_password"
                 />
-                <label class="form-label" for="form2Example1">Repeat Password</label>
               </div>
             </div>
           </div>
