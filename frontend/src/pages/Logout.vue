1e756f5846a17bac6b6db11088d4b1544cdc21b0
Isira Adithya <rp.isira@gmail.com>
2024-02-11 18:01:44 +0530

    Minor changes to Logout.Vue

--- frontend/src/pages/Logout.vue
+++ frontend/src/pages/Logout.vue
@@ -21,10 +21,14 @@ export default {
     Notiflix.Loading.standard();
     axios.delete("/api/auth/logout").then((data)=>{
       Notiflix.Loading.remove();
-      Notiflix.Report.success("Logout Success");
+      Notiflix.Report.success("Logout Success", "You can safely navigate away from the website now.", "OK", () => {
+        this.$router.push("/");
+      });
     }).catch((err) => {
       Notiflix.Loading.remove();
-      Notiflix.Report.success("Logout Success");
+      Notiflix.Report.failure("Something went wrong", "Please try again later.", "OK", () => {
+        this.$router.push("/");
+      });
     })
   },
   data() {
