21417d345e16c2c00edbfd04b81d73c89cd89d5f
Nesithp-Perera <nesithperera.pro@gmail.com>
2024-02-16 16:48:24 +0530

    Did header in vue

--- frontend/src/Template.vue
+++ frontend/src/Template.vue
@@ -1,35 +1,53 @@
 <template>
   <div>
-    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-      <a class="navbar-brand mx-4" href="#">EventHive</a>
-      <button
-        class="navbar-toggler"
-        type="button"
-        data-toggle="collapse"
-        data-target="#navbarNavAltMarkup"
-        aria-controls="navbarNavAltMarkup"
-        aria-expanded="false"
-        aria-label="Toggle navigation"
-      >
-        <span class="navbar-toggler-icon"></span>
-      </button>
-      <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
-        <div class="navbar-nav">
-          <router-link class="nav-item nav-link" to="/">Home</router-link>
-          <router-link v-if="!isLoggedIn" class="nav-item nav-link" to="/login">Login</router-link>
-          <router-link v-if="isLoggedIn" class="nav-item nav-link" to="/logout">Logout</router-link>
+    <div class="user-navbar">
+    <nav class="navbar navbar-expand-lg navbar-dark bg-transparent">
+        <div class="container">
+          <a class="navbar-brand" href="#">
+            <img src="./assets/images/event hive (1).png" alt="logo" class="custom-logo">
+          </a>
+          <button class="navbar-toggler shadow-none border-0" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasNavbar" aria-controls="offcanvasNavbar" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="sidebar offcanvas offcanvas-start" :style="sidebarStyles" tabindex="-1" id="offcanvasNavbar" aria-labelledby="offcanvasNavbarLabel">
+            <div class="offcanvas-header">
+              <h5 class="offcanvas-title" id="offcanvasNavbarLabel">
+                <img src="./assets/images/event hive (1).png" alt="logo" class="custom-logo">
+              </h5>
+              <button type="button" class="btn-close btn-close-white shadow-none border-0 " data-bs-dismiss="offcanvas" aria-label="Close"></button>
+            </div>
+            <div class="offcanvas-body d-flex flex-column flex-lg-row p-4 ">
+              <ul class="navbar-nav justify-content-center align-items-center fs-5 flex-grow-1 pe-3">
+                <li class="nav-item mx-2">
+                  <a class="nav-link text-white " href="home.html">Home</a>
+                </li>
+                <li class="nav-item mx-2">
+                  <a class="nav-link text-white " href="events">Events</a>
+                </li>
+                <li class="nav-item mx-2">
+                  <a class="nav-link text-white " href="about_us.html">About Us</a>
+                </li>
+                <li class="nav-item mx-2">
+                  <a class="nav-link text-white " href="contact_us">Contact Us</a>
+                </li>
+              </ul>    
+              <div class="d-flex flex-column flex-lg-row justify-content-center align-items-center gap-3">
+                <router-link  v-if="!isLoggedIn" class="text-white text-decoration-none px-3 py-1 button-color rounded-1" to="/login">LOG IN</router-link>
+                <router-link  v-if="isLoggedIn" class="text-white text-decoration-none px-3 py-1 button-color rounded-1" to="/logout">LOG OUT</router-link>
+                <router-link  class="text-white text-decoration-none px-3 py-1 button-color rounded-1" to="/signup">SIGN UP</router-link>
+              </div>      
+            </div>
+          </div>
         </div>
-      </div>
-    </nav>
+      </nav>
+    </div>
 
     <div class="mx-4">
       <router-view></router-view>
     </div>
 
     <div class="mx-4">
-      <footer
-        class="d-flex flex-wrap justify-content-between align-items-center py-3 my-2 border-top"
-      >
+      <footer class="d-flex flex-wrap justify-content-between align-items-center py-3 my-2 border-top">
         <p class="col-md-4 mb-0 text-body-secondary">
           &copy; 2024 EventHive, Inc
         </p>
@@ -130,14 +148,45 @@ export default {
       isLoggedIn: false,
     };
   },
-};
+  setup() {
+    const sidebarStyles = {
+      transition: 'transform 0.3s ease',
+      backgroundColor: 'rgba(0, 0, 0, 1)',
+      '@media only screen and (max-width: 768px)': { 
+        backgroundColor: 'rgba(0, 0, 0)',
+        
+      },
+    };
+
+      return {
+        sidebarStyles,
+      };
+    },
+  };
 </script>
   
 <style>
-@import url("https://fonts.googleapis.com/css2?family=Poppins&display=swap");
+  @import url("https://fonts.googleapis.com/css2?family=Poppins&display=swap");
 
 #app {
   font-family: "Poppins", sans-serif;
 }
+
+.user-navbar{
+  background-color: rgb(0, 0, 0)
+}
+.button-color{
+  background-color: #111F4D;
+}
+.custom-logo{
+    height: stretch;
+    width: 120px;
+    margin: 0px !important;
+    padding: 0px !important;
+}
+.nav-link:hover{
+    color: rgb(207, 206, 206) !important;
+}
+
 </style>
   
