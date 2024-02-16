4ce26e3050d212e26f1b62d20169311cf16aad1e
Isira Adithya <rp.isira@gmail.com>
2024-02-16 20:41:57 +0530

    Added /geocode api

--- backend/routes/common/geoapify/index.js
+++ backend/routes/common/geoapify/index.js
@@ -1,6 +1,22 @@
 import express from "express";
+import axios from "axios";
+import config from '../../../config.js';
 const router = express.Router();
 
-router.get("/geocode", async (req, res) => {});
+router.get("/geocode", async (req, res) => {
+    try {
+        const response = await axios.get("https://api.geoapify.com/v1/geocode/search?text=" + req.query['address'] + "&apiKey=" + config.GEOAPIFY_API_KEY)
+        res.json({
+            success: true,
+            data: response.data
+        })
+    } catch (err){
+        res.status(500);
+        return res.json({
+            success: false,
+            msg: "Internal Server Error"
+        })
+    }
+});
 
 export default router;
