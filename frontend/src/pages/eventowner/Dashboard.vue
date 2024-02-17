d3be77546b64e05670592131514f04692bd922a0
Isira Adithya <rp.isira@gmail.com>
2024-02-17 19:12:19 +0530

    Added a sidebar to the eventowner dashboard

--- frontend/src/pages/eventowner/Dashboard.vue
+++ frontend/src/pages/eventowner/Dashboard.vue
@@ -1,24 +1,46 @@
 <template>
-  <div class="container mt-5">
+  <div>
     <div
-      class="d-flex flex-column flex-shrink-0 p-3 text-white bg-dark"
+      class="d-flex flex-column flex-shrink-0 p-3 text-white bg-black"
       style="width: 280px"
     >
       <hr />
       <ul class="nav nav-pills flex-column mb-auto">
         <li class="nav-item">
-          <router-link :to="'/test'" href="#" class="nav-link active" aria-current="page">
-            Home
+          <router-link
+            :to="'/eventowner/dashboard/analytics'"
+            href="#"
+            class="nav-link active"
+            aria-current="page"
+          >
+            Analytics
           </router-link>
         </li>
+
         <li class="nav-item">
-            <router-link :to="'/test'" href="#" class="nav-link text-white" aria-current="page">
-            Events
+          <router-link
+            :to="'/eventowner/dashboard/add-event'"
+            href="#"
+            class="nav-link text-white"
+            aria-current="page"
+          >
+            Add Event
           </router-link>
         </li>
+
+        <li class="nav-item">
+          <router-link
+            :to="'/eventowner/dashboard/events'"
+            href="#"
+            class="nav-link text-white"
+            aria-current="page"
+          >
+            Manage Events
+          </router-link>
+        </li>
+        
       </ul>
       <hr />
-      
     </div>
   </div>
 </template>
