0e40fde21a4de579d7c49b61ed3afe33c962da7e
Isira Adithya <rp.isira@gmail.com>
2024-02-17 19:28:39 +0530

    Added eventowner analytics page

---
+++ frontend/src/pages/eventowner/subpages/EventOwnerAnalytics.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="container mt-5">
+        Analytics
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    name: "EventOwnerAnalyticsVue",
+    components: {},
+    mounted() {
+    },
+    data() {
+      return {
+      };
+    },
+    methods: {
+      test() {
+        alert(1)
+      }
+    },
+  };
+  </script>
