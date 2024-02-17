71ea0f53c2153bb876db1a223046779a183907d2
Isira Adithya <rp.isira@gmail.com>
2024-02-17 19:28:30 +0530

    Added event related sub-pages to event owner dashboard

---
+++ frontend/src/pages/eventowner/subpages/AddEvent.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="container mt-5">
+        Add Event
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    name: "EventOwnerAddEventVue",
+    components: {},
+    mounted() {
+    },
+    data() {
+      return {
+      };
+    },
+    methods: {
+      test() {
+        alert(1)
+      }
+    },
+  };
+  </script>
---
+++ frontend/src/pages/eventowner/subpages/Events.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="container mt-5">
+        Events
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    name: "EventOwnerEventsVue",
+    components: {},
+    mounted() {
+    },
+    data() {
+      return {
+      };
+    },
+    methods: {
+      test() {
+        alert(1)
+      }
+    },
+  };
+  </script>
