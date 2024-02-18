9898214120f2e3ba93f15a5199cb14a6c5796853
Isira Adithya <rp.isira@gmail.com>
2024-02-18 14:58:57 +0530

    Added a new file called ImagesCarousel.vue and mod ified AddEvent.vue

---
+++ frontend/src/components/ImagesCarousel.vue
@@ -0,0 +1,24 @@
+<template>
+    <div>
+        Lmao
+    </div>
+</template>
+  
+  <script>
+export default {
+  name: "ImageCarouselVue",
+  props: {
+    images: {
+      type: Array,
+      required: true,
+    }
+  },
+  components: {},
+  mounted() {},
+  data() {
+    return {};
+  },
+  methods: {
+  },
+};
+</script>
