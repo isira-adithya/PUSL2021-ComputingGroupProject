68a1fd602f803680e1006dbad3ce0eda96064412
Isira Adithya <rp.isira@gmail.com>
2024-02-29 21:28:48 +0530

    Added new migrations

---
+++ backend/migrations/20240229155835_/migration.sql
@@ -0,0 +1,145 @@
+-- CreateTable
+CREATE TABLE `PhoneNumber` (
+    `phone_id` INTEGER NOT NULL AUTO_INCREMENT,
+    `number` VARCHAR(191) NOT NULL,
+    `is_verified` BOOLEAN NOT NULL DEFAULT false,
+    `verified_at` DATETIME(3) NULL,
+    `verification_code` VARCHAR(191) NULL,
+
+    PRIMARY KEY (`phone_id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `EmailAddress` (
+    `email_id` INTEGER NOT NULL AUTO_INCREMENT,
+    `email` VARCHAR(191) NOT NULL,
+    `is_verified` BOOLEAN NOT NULL DEFAULT false,
+    `verification_code` VARCHAR(191) NULL,
+    `verified_at` DATETIME(3) NULL,
+
+    PRIMARY KEY (`email_id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `User` (
+    `user_id` INTEGER NOT NULL AUTO_INCREMENT,
+    `user_name` VARCHAR(191) NOT NULL,
+    `first_name` VARCHAR(191) NOT NULL,
+    `last_name` VARCHAR(191) NOT NULL,
+    `address` VARCHAR(191) NOT NULL,
+    `addr_geocoordinates` VARCHAR(191) NULL DEFAULT '0,0',
+    `notification_preference` VARCHAR(191) NOT NULL,
+    `password` VARCHAR(191) NOT NULL,
+    `profile_image` VARCHAR(191) NULL,
+    `phone_id` INTEGER NOT NULL,
+    `email_id` INTEGER NOT NULL,
+    `role` VARCHAR(191) NOT NULL DEFAULT 'VISITOR',
+    `is_active` BOOLEAN NOT NULL DEFAULT false,
+    `is_verified` BOOLEAN NOT NULL DEFAULT false,
+
+    UNIQUE INDEX `User_user_name_key`(`user_name`),
+    PRIMARY KEY (`user_id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `PasswordResetToken` (
+    `preset_token_id` INTEGER NOT NULL AUTO_INCREMENT,
+    `token` VARCHAR(191) NOT NULL,
+    `user_id` INTEGER NOT NULL,
+    `createdAt` INTEGER NOT NULL,
+
+    UNIQUE INDEX `PasswordResetToken_token_key`(`token`),
+    UNIQUE INDEX `PasswordResetToken_user_id_key`(`user_id`),
+    PRIMARY KEY (`preset_token_id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `Notification` (
+    `notification_id` INTEGER NOT NULL AUTO_INCREMENT,
+    `content` VARCHAR(512) NULL,
+    `notification_status` VARCHAR(191) NULL,
+    `user_id` INTEGER NULL,
+
+    PRIMARY KEY (`notification_id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `Verification` (
+    `verification_id` INTEGER NOT NULL AUTO_INCREMENT,
+    `verification_notes` VARCHAR(191) NULL,
+    `verification_status` VARCHAR(191) NULL,
+    `face_image_link` VARCHAR(191) NULL,
+    `nicback_image_link` VARCHAR(191) NULL,
+    `nicfront_image_link` VARCHAR(191) NULL,
+    `owner_id` INTEGER NULL,
+
+    PRIMARY KEY (`verification_id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `Commentary` (
+    `commentary_id` INTEGER NOT NULL AUTO_INCREMENT,
+    `user_id` INTEGER NULL,
+    `event_id` INTEGER NULL,
+    `comment` VARCHAR(512) NULL,
+    `rating` VARCHAR(191) NULL,
+
+    PRIMARY KEY (`commentary_id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `Event` (
+    `event_id` INTEGER NOT NULL AUTO_INCREMENT,
+    `name` VARCHAR(191) NULL,
+    `date_time` DATETIME(3) NULL,
+    `description` VARCHAR(8096) NULL,
+    `venue` VARCHAR(191) NULL,
+    `images` VARCHAR(2048) NULL,
+    `category` VARCHAR(191) NULL,
+    `location` VARCHAR(191) NULL,
+    `location_geocoordinates` VARCHAR(191) NULL DEFAULT '0,0',
+    `visibility` VARCHAR(191) NOT NULL DEFAULT 'private',
+    `uuid` VARCHAR(191) NOT NULL,
+    `owner_id` INTEGER NULL,
+
+    UNIQUE INDEX `Event_uuid_key`(`uuid`),
+    PRIMARY KEY (`event_id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `Ticket` (
+    `ticket_id` INTEGER NOT NULL AUTO_INCREMENT,
+    `price` INTEGER NOT NULL,
+    `name` VARCHAR(191) NOT NULL,
+    `description` VARCHAR(512) NOT NULL,
+    `event_id` INTEGER NOT NULL,
+
+    PRIMARY KEY (`ticket_id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- AddForeignKey
+ALTER TABLE `User` ADD CONSTRAINT `User_phone_id_fkey` FOREIGN KEY (`phone_id`) REFERENCES `PhoneNumber`(`phone_id`) ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `User` ADD CONSTRAINT `User_email_id_fkey` FOREIGN KEY (`email_id`) REFERENCES `EmailAddress`(`email_id`) ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `PasswordResetToken` ADD CONSTRAINT `PasswordResetToken_user_id_fkey` FOREIGN KEY (`user_id`) REFERENCES `User`(`user_id`) ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `Notification` ADD CONSTRAINT `Notification_user_id_fkey` FOREIGN KEY (`user_id`) REFERENCES `User`(`user_id`) ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `Verification` ADD CONSTRAINT `Verification_owner_id_fkey` FOREIGN KEY (`owner_id`) REFERENCES `User`(`user_id`) ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `Commentary` ADD CONSTRAINT `Commentary_user_id_fkey` FOREIGN KEY (`user_id`) REFERENCES `User`(`user_id`) ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `Commentary` ADD CONSTRAINT `Commentary_event_id_fkey` FOREIGN KEY (`event_id`) REFERENCES `Event`(`event_id`) ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `Event` ADD CONSTRAINT `Event_owner_id_fkey` FOREIGN KEY (`owner_id`) REFERENCES `User`(`user_id`) ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `Ticket` ADD CONSTRAINT `Ticket_event_id_fkey` FOREIGN KEY (`event_id`) REFERENCES `Event`(`event_id`) ON DELETE RESTRICT ON UPDATE CASCADE;
