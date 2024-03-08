54a5da87ea79dec21c4b170527cfb91ef495bb0a
Isira Adithya <rp.isira@gmail.com>
2024-03-08 21:48:06 +0530

    fixed a frontend bug

--- backend/modules/sms/sendSms.js
+++ backend/modules/sms/sendSms.js
@@ -5,7 +5,7 @@ async function sendSms(to, message, contactFname = null, contactLname = null, co
     console.log(`Sending '${message}' to ${to}`);
 
     // In dev mode, API key is empty is if it is empty, sms should not be sent.
-    if (config.NOTIFYLK_APIKEY.length() <= 0){
+    if (config.NOTIFYLK_APIKEY.length <= 0){
         return "Message Sent";
     }
 
--- frontend/src/pages/auth/Login.vue
+++ frontend/src/pages/auth/Login.vue
@@ -62,7 +62,6 @@
       </div>
     </div>
   </div>
-</div>
 </template>
 
 <script>
