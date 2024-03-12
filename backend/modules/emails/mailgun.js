2b06fe2958c880cc5e815a69ba479b621aa0ea27
Isira Adithya <rp.isira@gmail.com>
2024-03-12 16:49:53 +0530

    Added email sending module

--- backend/modules/emails/mailgun.js
+++ backend/modules/emails/mailgun.js
@@ -1,15 +1,31 @@
 import FormData from 'form-data';
 import Mailgun from 'mailgun.js';
-import config from '../../config';
+import config from '../../config.js';
 const mailgun = new Mailgun(FormData);
 const mg = mailgun.client({username: 'api', key: config.MAILGUN_APIKEY});
 
-mg.messages.create('mg.eventhive.live', {
-	from: "Excited User <no-reply@eventhive.live>",
-	to: ["yaxodo8283@darkse.com"],
-	subject: "Hello",
-	text: "Testing some Mailgun awesomeness!",
-	html: "<h1>Testing some Mailgun awesomeness!</h1>"
-})
-.then(msg => console.log(msg)) // logs response data
-.catch(err => console.log(err)); // logs any error
+
+// Create a function to send emails
+async function sendEmail(to, subject, text, html) {
+    try {
+        const mgResponse = await mg.messages.create('mg.eventhive.live', {
+            from: "EventHive <no-reply@eventhive.live>",
+            to: to,
+            subject: subject,
+            text: text,
+            html: html
+        });
+        if (mgResponse.status === 200) {
+            return true;
+        } else {
+            return false;
+        }
+    } catch(e) {
+        console.log(e)
+        return false;
+    }
+}
+
+export {
+    sendEmail
+};
