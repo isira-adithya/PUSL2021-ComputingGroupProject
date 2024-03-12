621b85538ff63f3a197aea8645ff2cf42065d34e
Isira Adithya <rp.isira@gmail.com>
2024-03-12 14:28:16 +0530

    Comment Delete API added

--- backend/routes/common/comments/index.js
+++ backend/routes/common/comments/index.js
@@ -32,4 +32,47 @@ router.post('/new', isLoggedIn, async (req, res) => {
     }
 });
 
+router.delete('/:id', isLoggedIn, async (req, res) => {
+    const comment_id = req.params.id;
+    const user_id = req.session.user_id;
+
+    // Check if the comment_id is a number
+    if (isNaN(comment_id)) {
+        return res.status(400).json({
+            success: false,
+            msg: "Invalid comment_id"
+        });
+    }
+
+    try {
+        const comment = await prisma.comment.findFirst({
+            where: {
+                comment_id: parseInt(comment_id)
+            }
+        });
+        if (comment.user_id !== user_id) {
+            res.status(401).json({
+                success: false,
+                msg: "Unauthorized"
+            });
+        } else {
+            await prisma.comment.delete({
+                where: {
+                    comment_id: parseInt(comment_id)
+                }
+            });
+            return res.status(200).json({
+                success: true,
+                msg: "Comment deleted"
+            });
+        }
+    } catch (error) {
+        console.log(error)
+        return res.status(500).json({
+            success: false,
+            msg: "Something went wrong"
+        });
+    }
+});
+
 export default router;
