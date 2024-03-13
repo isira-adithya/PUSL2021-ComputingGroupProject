dfa54369fdcd62db2af04d9778af34694567105f
Isira Adithya <rp.isira@gmail.com>
2024-03-13 13:37:47 +0530

    Integrated the Admin Users API

--- backend/routes/admin/index.js
+++ backend/routes/admin/index.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import EventsRouter from './events/events.js';
+import UsersRouter from './users/users.js';
 const router = express.Router();
 
 // Using middleware to check authentication and authorization of /admin
@@ -23,6 +24,7 @@ const checkAuth = (req, res, next) => {
 
 router.use(checkAuth);
 router.use("/events", EventsRouter);
+router.use("/users", UsersRouter);
 
 
 export default router;
