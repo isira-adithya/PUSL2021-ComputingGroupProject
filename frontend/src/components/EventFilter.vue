97307bfe8cfd9ca1ebd43d81fdd33506635657d8
Isira Adithya <rp.isira@gmail.com>
2024-03-16 15:45:28 +0530

    Added realtime location filter

--- frontend/src/components/EventFilter.vue
+++ frontend/src/components/EventFilter.vue
@@ -3,7 +3,13 @@
     <div class="row mb-3">
       <div class="col">
         <label for="location" class="form-label">Location:</label>
-        <input id="location" v-model="filters.location" class="form-control" placeholder="Location">
+        <input
+          id="location"
+          v-model="filters.location"
+          class="form-control"
+          placeholder="Location"
+          @keyup="filters.geoCoordinates = ''"
+        />
       </div>
       <div class="col">
         <label for="eventType" class="form-label">Event Type:</label>
@@ -20,53 +26,68 @@
       <div class="col">
         <label class="form-label">Date Range:</label>
         <div class="input-group">
-          <input type="date" v-model="filters.startDate" class="form-control" placeholder="Start Date">
+          <input
+            type="date"
+            v-model="filters.startDate"
+            class="form-control"
+            placeholder="Start Date"
+          />
           <span class="input-group-text">to</span>
-          <input type="date" v-model="filters.endDate" class="form-control" placeholder="End Date">
+          <input
+            type="date"
+            v-model="filters.endDate"
+            class="form-control"
+            placeholder="End Date"
+          />
         </div>
       </div>
       <div class="col">
-        <button class="btn btn-primary" @click="findNearMe">Find Events Near Me</button>
+        <button class="btn btn-primary" @click="findNearMe">
+          Find Events Near Me
+        </button>
       </div>
     </div>
   </div>
 </template>
 
 <script>
-import Notiflix from 'notiflix';
+import { filter } from "lodash";
+import Notiflix from "notiflix";
 
 export default {
   data() {
     return {
       filters: {
-        location: '',
-        eventType: '',
-        startDate: '',
-        endDate: ''
+        location: "",
+        eventType: "",
+        startDate: "",
+        endDate: "",
+        geoCoordinates: "",
       },
-      url_query: '',
-      userCoordinates: null
-    }
+      url_query: "",
+    };
   },
   mounted() {
     this.buildQuery();
   },
   watch: {
     filters: {
-      handler() {
+      handler(e) {
         this.buildQuery();
       },
-      deep: true
-    }
+      deep: true,
+    },
   },
   methods: {
     buildQuery() {
-      const { location, eventType, startDate, endDate } = this.filters;
+      const { location, eventType, startDate, endDate, geoCoordinates } =
+        this.filters;
       const query = new URLSearchParams({
         ...(location && { location }),
         ...(eventType && { eventType }),
         ...(startDate && { startDate }),
         ...(endDate && { endDate }),
+        ...(geoCoordinates && { geoCoordinates }),
       }).toString();
       this.url_query = query;
       return query;
@@ -75,25 +96,40 @@ export default {
       if (navigator.geolocation) {
         navigator.geolocation.getCurrentPosition(
           (position) => {
-            this.userCoordinates = {
+            this.filters.geoCoordinates = JSON.stringify({
               latitude: position.coords.latitude,
-              longitude: position.coords.longitude
-            };
+              longitude: position.coords.longitude,
+            });
+
+            // Reset this.filter.location
+            this.filters.location = "";
+
             // You can now use this.userCoordinates to filter events near the user's location
-            console.log('User coordinates:', this.userCoordinates);
+            console.log("User coordinates:", this.filters.geoCoordinates);
           },
           (error) => {
-            console.error('Error getting location:', error);
-            Notiflix.Notify.Failure('Error getting location:', error.message);
+            console.error("Error getting location:", error);
+
+            this.filters.geoCoordinates = JSON.stringify({
+              latitude: 8.4074843,
+              longitude: 81.1450867,
+            });
+
+            // Reset this.filter.location
+            this.filters.location = "";
+
+            Notiflix.Notify.failure("Error getting location:", error.message);
           }
         );
       } else {
-        console.error('Geolocation is not supported by this browser.');
-        Notiflix.Notify.Failure('Geolocation is not supported by this browser.');
+        console.error("Geolocation is not supported by this browser.");
+        Notiflix.Notify.failure(
+          "Geolocation is not supported by this browser."
+        );
       }
-    }
-  }
-}
+    },
+  },
+};
 </script>
 
 <style scoped>
