35101c534ac4c212e891e4994105b40619b93efe
Isira Adithya <rp.isira@gmail.com>
2024-03-17 21:34:31 +0530

    Database migrations

---
+++ backend/migrations/20240317151849_added_created_at_to_notifications/migration.sql
@@ -0,0 +1,2 @@
+-- AlterTable
+ALTER TABLE `notification` ADD COLUMN `created_at` DATETIME(3) NULL DEFAULT CURRENT_TIMESTAMP(3);
---
+++ backend/migrations/20240317153021_added_content_hash_to_emails/migration.sql
@@ -0,0 +1,2 @@
+-- AlterTable
+ALTER TABLE `notification` ADD COLUMN `content_hash` VARCHAR(512) NULL;
