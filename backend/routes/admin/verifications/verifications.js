ec8d7317114905f57935f69298b376563a5989a5
Isira Adithya <rp.isira@gmail.com>
2024-03-19 15:01:38 +0530

    Verifications Admin API responds with user details as well

--- backend/routes/admin/verifications/verifications.js
+++ backend/routes/admin/verifications/verifications.js
@@ -9,6 +9,26 @@ const router = express.Router();
 // Get all verifications
 router.get('/', async (req, res) => {
     const verifications = await prisma.verification.findMany();
+    for (let i = 0; i < verifications.length; i++) {
+        const user = await prisma.user.findUnique({
+            where: {
+                user_id: verifications[i].owner_id
+            }
+        });
+        const emailAddress = await prisma.emailAddress.findUnique({
+            where: {
+                email_id: user.email_id
+            }
+        });
+        const phoneNumber = await prisma.phoneNumber.findUnique({
+            where: {
+                phone_id: user.phone_id
+            }
+        });
+        verifications[i].user = user;
+        verifications[i].user.email = emailAddress;
+        verifications[i].user.phoneNumber = phoneNumber;
+    }
     res.json(verifications);
 });
 
