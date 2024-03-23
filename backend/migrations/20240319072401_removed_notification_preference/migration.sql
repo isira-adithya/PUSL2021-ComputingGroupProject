3728f3a0c03794fb70251522e1c743b8047f454a
Isira Adithya <rp.isira@gmail.com>
2024-03-23 13:08:17 +0530

    Removed DB Changes/Migrations

--- backend/migrations/20240319072401_removed_notification_preference/migration.sql
+++
@@ -1,9 +0,0 @@
-/*
-  Warnings:
-
-  - You are about to drop the column `is_email_notifications_enabled` on the `user` table. All the data in the column will be lost.
-
-*/
--- AlterTable
-ALTER TABLE `user` DROP COLUMN `is_email_notifications_enabled`,
-    MODIFY `notification_preference` VARCHAR(191) NOT NULL DEFAULT 'NONE';
--- backend/migrations/20240319092115_added_createdat_attribute_to_verifications/migration.sql
+++
@@ -1,2 +0,0 @@
--- AlterTable
-ALTER TABLE `verification` ADD COLUMN `created_at` DATETIME(3) NULL DEFAULT CURRENT_TIMESTAMP(3);
