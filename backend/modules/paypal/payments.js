c3077f88540db60f150c55295772d4ea288946fc
Isira Adithya <rp.isira@gmail.com>
2024-03-23 19:54:04 +0530

    Add PayPal payment module

---
+++ backend/modules/paypal/payments.js
@@ -0,0 +1,42 @@
+import config from '../../config.js';
+import PayPalRestSDK from 'paypal-rest-sdk';
+
+PayPalRestSDK.configure({
+    'mode': 'sandbox', //sandbox or live
+    'client_id': config.PAYPAL_CI,
+    'client_secret': config.PAYPAL_SK
+})
+
+async function getApprovalLink(amount, description, return_url, cancel_url) {
+    return new Promise((resolve, reject) => {
+        const payment = {
+            "intent": "sale",
+            "payer": {
+                "payment_method": "paypal"
+            },
+            "transactions": [{
+                "amount": {
+                    "total": amount,
+                    "currency": "USD"
+                },
+                "description": description
+            }],
+            "redirect_urls": {
+                "return_url": return_url,
+                "cancel_url": cancel_url
+            }
+        };
+
+        PayPalRestSDK.payment.create(payment, function (error, payment) {
+            if (error) {
+                reject(error);
+            } else {
+                resolve(payment.links[1].href);
+            }
+        });
+    });
+}
+
+export {
+    getApprovalLink
+}
