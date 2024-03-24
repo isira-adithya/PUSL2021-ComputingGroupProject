cf2535d3d447a0fdfff57de8dfe70d803f740528
Isira Adithya <rp.isira@gmail.com>
2024-03-24 14:45:40 +0530

    Update Login.vue form-check visibility

--- frontend/src/pages/auth/Login.vue
+++ frontend/src/pages/auth/Login.vue
@@ -19,7 +19,7 @@
               <input name="password" type="password" class="form-control" v-model="password" />
             </div>
 
-            <div class="form-check mb-3 mt-2 ms-4" >
+            <div class="form-check mb-3 mt-2 ms-4" v-if="false">
                 <input
                   class="form-check-input"
                   type="checkbox"
