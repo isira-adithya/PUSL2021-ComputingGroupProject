63a9f2af8d43273a82a29dfd77333ee5498e77d6
Isira Adithya <rp.isira@gmail.com>
2024-03-24 14:09:22 +0530

    Add delete route for tickets

--- backend/routes/admin/tickets/index.js
+++ backend/routes/admin/tickets/index.js
@@ -33,4 +33,38 @@ router.get('/', async (req, res) => {
     return res.json(tickets);
 });
 
+router.delete("/:ticket_id", async (req, res) => {
+    
+    // check if the ticket_id is a number
+    if (isNaN(req.params.ticket_id)) {
+        res.status(400);
+        return res.json({
+            success: false,
+            msg: "Invalid ticket id."
+        });
+    }
+    
+    const ticket_id = parseInt(req.params.ticket_id);
+
+    try {
+        const result = await prisma.ticket.delete({
+            where: {
+                ticket_id: ticket_id,
+            }
+        });
+        console.log(result);
+        return res.json({
+            success: true,
+            msg: "Ticket deleted successfully."
+        });
+    } catch (error) {
+        console.error(error);
+        res.status(500);
+        return res.json({
+            success: false,
+            msg: "Failed to delete ticket."
+        });
+    }
+});
+
 export default router;
