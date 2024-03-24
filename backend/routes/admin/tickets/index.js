7071f6977b22375dc53d9664c23b1935decf1f48
Isira Adithya <rp.isira@gmail.com>
2024-03-24 14:23:42 +0530

    Add route to retrieve ticket payments

--- backend/routes/admin/tickets/index.js
+++ backend/routes/admin/tickets/index.js
@@ -33,6 +33,82 @@ router.get('/', async (req, res) => {
     return res.json(tickets);
 });
 
+router.get("/payments/:id", async (req, res) => {
+
+    // check if the ticket_id is a number
+    if (isNaN(req.params.id)) {
+        res.status(400);
+        return res.json({
+            success: false,
+            msg: "Invalid ticket id."
+        });
+    }
+
+    const ticket = await prisma.ticket.findUnique({
+        where: {
+            ticket_id: parseInt(req.params.id)
+        },
+        select: {
+            ticket_id: true,
+            event: {
+                select: {
+                    name: true,
+                    event_id: true,
+                    images: true
+                }
+            },
+            price: true,
+            name: true,
+            description: true
+        }
+    })
+    if (ticket == null) {
+        res.status(400);
+        return res.json({
+            success: false,
+            msg: "Ticket not found."
+        });
+    }
+
+    const payments = await prisma.ticketReceipt.findMany({
+        where: {
+            ticket_id: ticket.ticket_id
+        },
+        select: {
+            cost: true,
+            created_at: true,
+            payment_method: true,
+            ticket_code: true,
+            payment: {
+                select: {
+                    status: true,
+                    created_at: true,
+                    amount: true,
+                    user: {
+                        select: {
+                            user_name: true,
+                            first_name: true,
+                            last_name: true,
+                            profile_image: true,
+                            email_address: {
+                                select: {
+                                    email: true,
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    });
+
+    return res.json({
+        success: true,
+        ticket: ticket,
+        payments: payments
+    });
+});
+
 router.delete("/:ticket_id", async (req, res) => {
     
     // check if the ticket_id is a number
