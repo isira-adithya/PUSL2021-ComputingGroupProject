62718b6bd79eafac31d415e3ad1a8c633aa8f27b
Isira Adithya <rp.isira@gmail.com>
2024-03-24 13:04:52 +0530

    Add route to get tickets owned by user

--- backend/routes/eventowner/index.js
+++ backend/routes/eventowner/index.js
@@ -128,6 +128,17 @@ router.get("/verification-status", async (req, res) => {
     
 })
 
+router.get("/tickets", async (req, res) => {
+    const tickets = await prisma.ticket.findMany({
+        where: {
+            event: {
+                owner_id: req.session.user_id
+            }
+        }
+    })
+    return res.json(tickets);
+});
+
 // routes
 router.use("/mobile-verification", MobileVerificationRouter);
 router.use("/file", FileUploadHandler);
